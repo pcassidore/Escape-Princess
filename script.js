27cb6bfd1f09be016a3cfb802953318f27009ae7
Rafaela0306 <rafaelatanakacardoso@gmail.com>
2025-06-04 10:53:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+// Função para tocar ou pausar a música e alternar o ícone de som
 function tocarSom() {
-    var audio = document.getElementById("myAudio");
-    var icone = document.getElementById("iconeSom");
+    var audio = document.getElementById("myAudio"); //a musica em mp3
+    var icone = document.getElementById("iconeSom"); //a imagem do icone de som
+    // Obtém o elemento de áudio pelo ID
 
+    // Verifica se o áudio está pausado
     if (audio.paused) {
-        audio.play();
-        icone.src = "imagens/icone_som.png"; // Ícone som ligado
+        audio.play(); // Toca a música
+        icone.src = "imagens/icone_som.png"; // Altera o ícone para som ligado
     } else {
-        audio.pause();
-        icone.src = "imagens/icone_semsom.png"; // Ícone som desligado
+        audio.pause(); // Pausa a música
+        icone.src = "imagens/icone_semsom.png"; // Altera o ícone para som desligado
     }
 }
